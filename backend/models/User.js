9da514d198f5acc0c8cb81a9f0efa90949e3efa6
Ranjitkar <67991569+sypase@users.noreply.github.com>
2025-04-10 19:23:06 -0700

    `feat(models): add credits field to User schema`

--- backend/models/User.js
+++ backend/models/User.js
@@ -26,6 +26,10 @@ const UserSchema = new mongoose.Schema(
             type: Number,
             default: 0
         },
+        credits: {
+            type: Number,
+            default: 0
+        },
     },
     {
         timestamps: true,
